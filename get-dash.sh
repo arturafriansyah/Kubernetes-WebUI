aec6811cac17ce072ce7ed938c351d3b185aae5d
arturafriansyah <42965691+arturafriansyah@users.noreply.github.com>
2023-08-22 14:55:58 +0700

    Update get-dash.sh for k3d deployment

--- get-dash.sh
+++ get-dash.sh
@@ -7,13 +7,31 @@
 # First version : Aug 2019
 # Last modified : Dec 2020
 
-export DASH_NODE_NAME=`kubectl get pods  --all-namespaces -o wide | grep "kubernetes-dashboard-" | awk {'print $8'}`
-export DASH_NODE_IP=`grep $DASH_NODE_NAME /etc/hosts | awk {'print $1'}`
-export DASH_NODE_PORT=` kubectl get services  --all-namespaces -o wide | grep "443:" | awk {'print $6'} | sed  's/443//' | sed 's/\/TCP//'`
-export TOKEN=`kubectl -n kubernetes-dashboard create token admin-user`
-echo "Your Dashboard with Nodeport URL is: https://$DASH_NODE_IP$DASH_NODE_PORT/"
-echo
-#
-# kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
-echo "Token: $TOKEN"
-echo
+#!/bin/bash
+
+# Check if the deployment uses k3d
+k3d=$(docker ps | grep server-0 | wc -l)
+
+# if output is 1 or true 
+if [ "$k3d" -eq 1 ]; then
+
+  export DASH_NODE_NAME=`kubectl get nodes | grep server-0 | awk '{print 1}'`
+  export DASH_NODE_IP=`kubectl describe nodes k3d-k3s-default-server-0 | grep InternalIP | awk '{print $2}'`
+  export DASH_NODE_PORT=`kubectl get svc -A | grep -i kubernetes-dashboard | grep "443:" | awk '{print $6}' | sed  's/443//' | sed 's/\/TCP//'`
+  export TOKEN=`kubectl -n kubernetes-dashboard create token admin-user`
+  echo "Your Dashboard with Nodeport URL is: https://$DASH_NODE_IP$DASH_NODE_PORT/"
+  echo
+  echo "Token: $TOKEN"
+  echo
+
+else
+
+  export DASH_NODE_NAME=`kubectl get pods  --all-namespaces -o wide | grep "kubernetes-dashboard-" | awk {'print $8'}`
+  export DASH_NODE_IP=`grep $DASH_NODE_NAME /etc/hosts | awk {'print $1'}`
+  export DASH_NODE_PORT=` kubectl get services  --all-namespaces -o wide | grep "443:" | awk {'print $6'} | sed  's/443//' | sed 's/\/TCP//'`
+  export TOKEN=`kubectl -n kubernetes-dashboard create token admin-user`
+  echo "Your Dashboard with Nodeport URL is: https://$DASH_NODE_IP$DASH_NODE_PORT/"
+  echo
+  echo "Token: $TOKEN"
+  echo
+fi
